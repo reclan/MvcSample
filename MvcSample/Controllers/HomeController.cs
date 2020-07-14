3c38d1c5712ee574f6ae62615a21f8e1a6aeb0d7
reclan <reclan@mail.ru>
2020-07-14 13:37:26 +0300

    About page added

--- MvcSample/Controllers/HomeController.cs
+++ MvcSample/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace MvcSample.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
